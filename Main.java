632d503300a6cb055218e428bebe78854d340080
James Perez <xoptoss@gmail.com>
2014-05-02 18:50:43 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+package mainPackage;
+
+import java.util.Scanner;
+
+public class Main {
+    public static boolean replay;
+         
+    public static void main(String[] args) {
+    
+        String input;
+        Scanner Keyboard = new Scanner(System.in);               
+        Dice dice = new Dice(1);
+        Game game = new Game();
+    
+        do{                                 
+            dice.Throw(dice.Bounces);
+            game.AddThrows();
+            dice.Value();      
+            System.out.println("Dice Landed on "+dice.value);
+            game.GetThrows();
+            game.Retry();          
+        }while(replay == true);        
+    }    
+}
